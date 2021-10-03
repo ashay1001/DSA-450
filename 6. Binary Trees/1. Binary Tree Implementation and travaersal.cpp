6984f06720eb912f3039f5133aad3e3b6350c715
Ashay <ashay165@gmail.com>
2021-10-03 17:40:08 +0530

    .

--- 6. Binary Trees/1. Binary Tree Implementation and travaersal.cpp
+++ 6. Binary Trees/1. Binary Tree Implementation and travaersal.cpp
@@ -115,13 +115,7 @@ void levelorderTraversal(Node *root)
 
 int main()
 {
-    // Node *root = createNode(1);
-    // root->left = createNode(2);
-    // root->right = createNode(3);
-    // root->left->left = createNode(4);
-    // root->left->right = createNode(5);
-    // root->right->left = createNode(6);
-    // root->right->right = createNode(7);
+
     Node *root = NULL;
 
     for(int i=1; i<=7; i++)
