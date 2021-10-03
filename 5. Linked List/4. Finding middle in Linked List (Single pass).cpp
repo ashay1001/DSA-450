26cda16ce142a7511244e16d52b1f21f625fa474
Ashay <ashay165@gmail.com>
2021-10-03 17:22:17 +0530

    .

--- 5. Linked List/4. Finding middle in Linked List (Single pass).cpp
+++ 5. Linked List/4. Finding middle in Linked List (Single pass).cpp
@@ -1,76 +1,19 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-int check()
-{
-	#ifndef ONLINE_JUDE
-		freopen("input.txt", "r", stdin);
-		freopen("output.txt", "w",stdout);
-	#endif
-}
-
-struct node
-{
-    node* next;
-    int data;
-};
-
-node* head = NULL;
-
-void appendNode(int n)
-{
-    if(head == NULL)
-    {
-        node* temp = new node();
-        temp->data = n;
-        temp->next = NULL;
-        head = temp;
-    }
-    else
-    {
-        node* temp = head;
-        while(temp->next)
-        {
-            temp = temp->next;
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        if(head->next == NULL) {return head;}
+        if(head->next->next == NULL) {return head->next;}
+        
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast->next != NULL && fast->next->next != NULL)
+        {  
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        node* t = new node();
-        temp->next = t;
-        t->data = n;
+        if(fast->next != NULL)
+            return slow->next;
+        else
+            return slow;
     }
-}
-
-void display()
-{
-    node* temp = head;
-    while(temp->next)
-    {
-        cout<<temp->data<<"->";
-        temp = temp->next;
-    }
-    cout<<temp->data<<endl;
-}
-
-void getMiddle()
-{
-    node* fast = head;
-    node* slow = head;
-    while(fast->next != NULL)
-    {  
-        slow = slow->next;
-        fast = fast->next->next;
-    }
-    cout<<"Middle Element: "<<slow->data;
-}
-
-int main()
-{   
-    check();
-    for(int i=1; i<=15; i++)
-    {
-        appendNode(i);
-    }
-    display();
-    getMiddle();
-
-    return 0;
-}
+};
---
+++ 5. Linked List/5. Check if Circular Linked List.cpp
@@ -0,0 +1,14 @@
+bool isCircular(Node *head)
+{
+   // Your code here
+   Node *temp =head;
+    while(temp->next != NULL)
+    {
+        if(temp->next == head)
+        {
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
