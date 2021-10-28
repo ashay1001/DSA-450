1dc407c61509657fc7327c3310d6fff56a99b4eb
Ashay <ashay165@gmail.com>
2021-10-28 16:00:52 +0530

    added Rotate matrix by 90

---
+++ 3. Strings/5. Palindrome String.cpp
@@ -0,0 +1,13 @@
+int isPalindrome(string S)
+	{
+	    // Your code goes here
+	    int l=0, r = S.length()-1;
+	    while(l<r)
+	    {
+	        if(S[l] != S[r])
+	            return 0;
+	       l++, r--;
+	    }
+	
+	    return 1;
+	}
